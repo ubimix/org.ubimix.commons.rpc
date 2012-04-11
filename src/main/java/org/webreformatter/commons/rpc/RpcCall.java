f97db88ddc66bcad1ce7ccd5dcccd6522e5860ff
mikhail.kotelnikov <none@none>
2012-04-11 16:51:28 +0200

    Some utility methods for RpcError were moved from to the RpcError class.

--- src/main/java/org/webreformatter/commons/rpc/RpcCall.java
+++ src/main/java/org/webreformatter/commons/rpc/RpcCall.java
@@ -17,51 +17,17 @@ import org.webreformatter.commons.json.rpc.RpcResponse;
 public abstract class RpcCall extends CallEvent<RpcRequest, RpcResponse> {
 
     /**
-     * Creates and returns a new {@link RpcError} instance using the information
-     * from the given exception
-     * 
-     * @param code the code of the error to set
-     * @param t the exception used as a source of information for the resulting
-     *        error object
-     * @return an {@link RpcError} instance filled with the information from the
-     *         given exception (or error)
+     * @see RpcError#getError(int, Throwable)
      */
     public static RpcError getError(int code, Throwable t) {
-        RpcError error = new RpcError();
-        StringBuilder buf = new StringBuilder();
-        StackTraceElement[] stackTrace = t.getStackTrace();
-        for (StackTraceElement e : stackTrace) {
-            buf.append("\n  ");
-            buf.append(e.getClassName()
-                + "#"
-                + e.getMethodName()
-                + " ("
-                + e.getFileName()
-                + ":"
-                + e.getLineNumber()
-                + ")");
-            buf.append(e);
-        }
-        buf.append("\n");
-        error
-            .setValue("code", code)
-            .setValue("message", t.getMessage())
-            .setValue("stackTrace", buf.toString());
-        return error;
+        return RpcError.getError(code, t);
     }
 
     /**
-     * Creates and returns a new {@link RpcError} instance using the information
-     * from the given exception. This method sets the
-     * {@link RpcError#ERROR_INTERNAL_ERROR} internal error code.
-     * 
-     * @param t the exception used as a source of information for the resulting
-     *        error object
-     * @return an {@link RpcError} instance filled with the information from the
-     *         given exception (or error)
+     * @see RpcError#getError(Throwable)
      */
     public static RpcError getError(Throwable t) {
-        return getError(RpcError.ERROR_INTERNAL_ERROR, t);
+        return RpcError.getError(t);
     }
 
     /**
